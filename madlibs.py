27aab3a055b0c8ad787806a4fa9a61a7396051e2
prizonizel <54692098+prizonizel@users.noreply.github.com>
2019-09-18 18:59:37 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+import random
+#%%
+noun = ["rock","paper","scissors"]
+verb = ["crushes","covers","cuts"]
+for x in range(5):
+    print(noun[random.randint(0,2)],verb[random.randint(0,2)],noun[random.randint(0,2)])
+
+
+#%%
